fbab0b51c7163e8e4c6b944821dd006c7657c7d8
Irwing <86335744+Irwingduran@users.noreply.github.com>
2024-04-08 23:59:48 -0600

    index.tsx

---
+++ index.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useRef, useState } from "react";
+
+const BASE_URL = " "; //PUBLIC API 
+
+interface Post {
+  id: number;
+  title: string;
+}
+
+export default function Demo() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+return (
+  
+)
+  }
